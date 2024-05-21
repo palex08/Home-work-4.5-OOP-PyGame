0d1bfe2474d692d38044c622c3cd3765d8306004
Alexander <82374540+palex08@users.noreply.github.com>
2024-05-22 04:15:07 +1000

    New Road, target collision funktion

--- main.py
+++ main.py
@@ -8,19 +8,19 @@ class MyCar:
         self.image = pygame.image.load('img/mycar.png')
         self.road_width = road_width
         self.road_height = road_height
-        self.x = self.road_width // 2 - self.image.get_width() // 2
+        self.x = 470 - self.image.get_width() // 2
         self.y = self.road_height - 150
         self.speed = speed  # Скорость перемещения машины
 
-    def draw(self, field_surface):
-        field_surface.blit(self.image, (self.x, self.y))
+    def draw(self, screen):
+        screen.blit(self.image, (self.x, self.y))
 
     def move_left(self):
-        if self.x - self.speed - 50 >= 0:
+        if self.x - self.speed >= 290:
             self.x -= self.speed
 
     def move_right(self):
-        if self.x + self.speed <= self.road_width - self.image.get_width() - 50:
+        if self.x + self.speed <= 650 - self.image.get_width():
             self.x += self.speed
 
 
@@ -31,7 +31,7 @@ class Trees:
         self.image = pygame.image.load('img/tree.png')
         self.speed = speed
         self.side_tree = random.choice([(0, self.SCREEN_WIDTH - 1030),
-                                        (self.SCREEN_WIDTH - 550, self.SCREEN_WIDTH - 380)])
+                                        (self.SCREEN_WIDTH - 500, self.SCREEN_WIDTH - 300)])
         self.x = random.randint(self.side_tree[0], self.side_tree[1])
         self.y = 0 - self.image.get_height()
         trees_list.append(self)
@@ -46,71 +46,60 @@ class Trees:
 
 
 class Road:
-    def __init__(self, road_width, road_height, speed=6):
-        self.road_width = road_width
-        self.road_height = road_height + 50
+    def __init__(self, SCREEN_WIDTH, SCREEN_HEIGHT, speed=6):
+        self.SCREEN_WIDTH = SCREEN_WIDTH
+        self.SCREEN_HEIGHT = SCREEN_HEIGHT
+        self.road_image = pygame.image.load("img/road.png")
+        self.x = 250
+        self.y = 0
         self.speed = speed
-        self.lane_width = 60
-        self.line_length = 30
-        self.line_spacing = 20
-        self.lanes = 6
-        self.lines = []
-        self.initialize_lines()
-
-    def initialize_lines(self):
-        self.lines = []
-        for lane in range(1, self.lanes):
-            x = (self.road_width // 2 - (self.lane_width * self.lanes // 2)) + lane * self.lane_width
-            for y in range(0, self.road_height, self.line_length + self.line_spacing):
-                self.lines.append((x, y))
-
-    def update(self):
-        for i in range(len(self.lines)):
-            x, y = self.lines[i]
-            y += self.speed
-            if y > self.road_height - self.line_length:
-                y = -self.line_length
-            self.lines[i] = (x, y)
-
-    def draw(self, road_surface):
-        for x, y in self.lines:
-            pygame.draw.line(road_surface, (255, 255, 255), (x, y), (x, y + self.line_length), 5)
-
-
-class Target:
-    def __init__(self, road_width, road_height, lanes, speed=3):
-        self.images = ['img/car1.png', 'img/car2.png', 'img/car3.png']
-        self.image = pygame.image.load(random.choice(self.images))
-        self.road_width = road_width
-        self.road_height = road_height
-        self.x = random.randint(0, road_width - self.image.get_width())
-        self.y = 0 - self.image.get_height()
-        self.lanes = lanes
-        self.speed = speed
-        self.lane_width = self.road_width // self.lanes
-        target_list.append(self)
-
-
 
     def update(self):
         self.y += self.speed
-        if self.y > self.road_height:
+        if self.y + self.road_image.get_height() > self.SCREEN_HEIGHT:
+            self.y = -self.road_image.get_height()
+
+    def draw(self, screen):
+        screen.blit(self.road_image, (self.x, self.y))
+        screen.blit(self.road_image, (self.x, self.y + self.road_image.get_height()))
+
+
+class Target:
+    def __init__(self, SCREEN_WIDTH, SCREEN_HEIGHT):
+        self.SCREEN_WIDTH = SCREEN_WIDTH
+        self.SCREEN_HEIGHT = SCREEN_HEIGHT
+        self.images = ['img/car1.png', 'img/car2.png', 'img/car3.png']
+        self.image = pygame.image.load(random.choice(self.images))
+        self.x = random.choice([310, 400, 490, 585])
+        self.y = 0 - self.image.get_height()
+        self.speed = random.randint(2, 5)
+        self.rect = self.image.get_rect(topleft=(self.x, self.y))
+        target_list.append(self)
+
+    def update(self):
+        self.y += self.speed
+        self.rect.topleft = (self.x, self.y)
+        if self.y > self.SCREEN_HEIGHT:
             target_list.remove(self)
 
-    def draw(self, field_surface):
-        field_surface.blit(self.image, (self.x, self.y))
+    def draw(self, screen):
+        screen.blit(self.image, (self.x, self.y))
+
+    def check_collision(self, other):
+        return self.rect.colliderect(other.rect)
 
 
 trees_list = []
 target_list = []
 
+
 def main():
-    time = 60
+    target_time = 60
+    tree_time = 60
     SCREEN_WIDTH = 1200
     SCREEN_HEIGHT = 650
 
-    road_width, road_height = 400, 650
-    lanes = 6  # Number of lanes on the road
+    road_width, road_height = 446, 650
     pygame.init()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -118,20 +107,23 @@ def main():
     icon = pygame.image.load("img/icon.jpg")
     pygame.display.set_icon(icon)
 
-    field_surface = pygame.Surface((road_width, road_height))
-    field_rect = field_surface.get_rect(topleft=(250, 0))
-
     clock = pygame.time.Clock()
     game_running = True
-    road = Road(road_width, road_height)
+    road = Road(SCREEN_WIDTH, SCREEN_HEIGHT)
     mycar = MyCar(road_width, road_height)
 
+    def can_spawn_target(x):
+        count = sum(1 for target in target_list if target.x == x and target.y < 300)
+        return count < 1
+
     while game_running:
-        delta_time = clock.tick(60)
+        delta_time = clock.tick(30)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 game_running = False
+            if event.type == pygame.MOUSEBUTTONUP:
+                print(pygame.mouse.get_pos())
 
         # Обработка нажатий клавиш
         keys = pygame.key.get_pressed()
@@ -141,33 +133,46 @@ def main():
             mycar.move_right()
 
         screen.fill('DarkGreen')
-        field_surface.fill('grey51')
 
-        if time == 0:
+        if tree_time == 0:
             tree = Trees(SCREEN_WIDTH, SCREEN_HEIGHT)
-            taeget = Target(SCREEN_WIDTH, SCREEN_HEIGHT, lanes)
-            time = random.randint(10, 60)
+            tree_time = random.randint(10, 60)
         else:
-            time -= 1
+            tree_time -= 1
+
+        if target_time == 0:
+            possible_x_positions = [310, 400, 490, 585]
+            random.shuffle(possible_x_positions)
+            for x in possible_x_positions:
+                if can_spawn_target(x):
+                    target = Target(SCREEN_WIDTH, SCREEN_HEIGHT)
+                    target.x = x
+                    target.rect.topleft = (target.x, target.y)
+                    break
+            target_time = random.randint(30, 60)
+        else:
+            target_time -= 1
+
+        road.update()
+        road.draw(screen)
 
         for target in target_list:
             target.update()
+            target.draw(screen)
+
+        for i in range(len(target_list)):
+            for j in range(i + 1, len(target_list)):
+                if target_list[i].check_collision(target_list[j]):
+                    target_list[i].speed = 4
+                    target_list[j].speed = 3
+
         for trees in trees_list:
             trees.update()
-        road.update()
-
-        road.draw(field_surface)
-
-        for trees in trees_list:
             trees.draw(screen)
-        mycar.draw(field_surface)
 
-        for target in target_list:
-            target.draw(field_surface)
+        mycar.draw(screen)
 
-        screen.blit(field_surface, field_rect.topleft)
-
-        pygame.display.flip()
+        pygame.display.update()
 
     pygame.quit()
     sys.exit()
